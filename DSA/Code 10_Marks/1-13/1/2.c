78d2404f9474b282bb00398d59430fbb521edc6d
TejSutar8 <tejsutar8@gmail.com>
2025-04-06 10:00:52 +0530

    #

--- DSA/Code 10_Marks/1-13/1/2.c
+++ DSA/Code 10_Marks/1-13/1/2.c
@@ -2,41 +2,55 @@
 
 #define MAX 10
 
-int adj[MAX][MAX]; 
-int n; 
+int adj[MAX][MAX];
+int n;
 
-void initializeGraph() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+void initializeGraph()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             adj[i][j] = 0;
         }
     }
 }
 
-void addEdge(int src, int dest) {
-    if (src >= 0 && src < n && dest >= 0 && dest < n) {
+void addEdge(int src, int dest)
+{
+    if (src >= 0 && src < n && dest >= 0 && dest < n)
+    {
         adj[src][dest] = 1;
-    } else {
+    }
+    else
+    {
         printf("Invalid edge! Source or destination out of range.\n");
     }
 }
 
-void displayMatrix() {
+void displayMatrix()
+{
     printf("\nAdjacency Matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             printf("%d\t", adj[i][j]);
         }
         printf("\n");
     }
 }
 
-void printIndegree() {
+void printIndegree()
+{
     printf("\nIndegree of vertices:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int indegree = 0;
-        for (int j = 0; j < n; j++) {
-            if (adj[j][i] == 1) {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj[j][i] == 1)
+            {
                 indegree++;
             }
         }
@@ -44,7 +58,8 @@ void printIndegree() {
     }
 }
 
-int main() {
+int main()
+{
     int edges, src, dest;
 
     printf("Enter the number of vertices: ");
@@ -56,7 +71,8 @@ int main() {
     scanf("%d", &edges);
 
     printf("Enter edges (source destination):\n");
-    for (int i = 0; i < edges; i++) {
+    for (int i = 0; i < edges; i++)
+    {
         scanf("%d %d", &src, &dest);
         addEdge(src, dest);
     }
